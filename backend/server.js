fdc704ae75a61783b323038c9d4bf7fd6329a1a1
Samin010 <saminbashir484@gmail.com>
2024-12-27 22:08:36 +0530

    "Sign up functionality"

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser';
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from './routes/message.routes.js'
@@ -11,6 +12,10 @@ import connectToMongodb from './db/connecToMongodb.js'
 const app=express()
 dotenv.config()
 
+app.use(cors({
+   origin:process.env.CLIENT_URL,
+   credentials:true
+}))
 app.use(express.json())
 app.use(cookieParser())
 const port=process.env.PORT || 5000
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,5 +5,15 @@ export default {
     extend: {},
   },
   // eslint-disable-next-line no-undef
-  plugins: [require("daisyui")],
+  plugins:[]
 };
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+// 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+// 	theme: {
+// 		extend: {},
+// 	},
+// 	// eslint-disable-next-line no-undef
+// 	plugins: [require("daisyui")],
+// };
