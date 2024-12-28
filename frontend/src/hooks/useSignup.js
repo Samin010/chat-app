83cadae2c4aae99a1b7b5f436ff9678f532ca318
Samin010 <saminbashir484@gmail.com>
2024-12-28 15:59:43 +0530

    "Login and Logout functionalities added"

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContest";
 
 const useSignup = () => {
   const [loading, setLoading] = useState(false);
+  const {setAuthUser}= useAuthContext()
   const signup = async ({
     fullname,
     username,
@@ -36,7 +38,16 @@ const useSignup = () => {
         }),
       });
       const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+      // local storage
+
       console.log(data);
+      
+      
+      localStorage.setItem("chat-user", JSON.stringify(data));
+      setAuthUser(data);
     } catch (error) {
       toast.error(error);
     } finally {
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,15 @@ import { createRoot ,ReactDOM} from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom' 
 import App from './App.jsx'
+import { AuthContextProvider } from './context/AuthContest.jsx'
 // import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+    <AuthContextProvider>
       <App />
+    </AuthContextProvider>
     </BrowserRouter>
   </StrictMode>
 );
