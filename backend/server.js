9d43ac7f3e6d99d2aa6f499a05f812e4b8fcff9d
Samin010 <saminbashir484@gmail.com>
2024-12-31 16:20:42 +0530

    'FINISHED'

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser';
 import cors from "cors";
-
+import path from 'path'
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from './routes/message.routes.js'
 import userRoutes from './routes/user.routes.js'
@@ -15,6 +15,9 @@ dotenv.config()
 //    origin:process.env.CLIENT_URL,
 //    credentials:true
 // }))
+
+const __dirname=path.resolve()
+
 app.use(express.json())
 app.use(cookieParser())
 const port=process.env.PORT || 5000
@@ -27,6 +30,12 @@ app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoutes)
 app.use('/api/users',userRoutes)
 
+
+app.use(express.static(path.join(__dirname,'/frontend/dist')))
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'frontend','dist','index.html'))
+})
 
 server.listen(port,()=>{
   connectToMongodb()
